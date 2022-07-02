f55971a0c7dbe969f59aa38e6efedd72bb6fed15
_GrandMaster_ MRF <fachruddin18genz@gmail.com>
2022-07-03 05:24:07 +0700

    fix bug socket

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const http = require("http");
 const { Server } = require("socket.io");
 
 const router = require("./src/routes");
 
-const app = express();
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
   },
 });
 
 require("./src/socket")(io);
 const port = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(cors());
 
 app.use("/api/v1/", router);
 app.use("/uploads", express.static("uploads"));
